d710a2d718ff91246b4bd810d648bade48e01843
Ilya Udovenko <41537543+iudovenko@users.noreply.github.com>
2021-11-11 14:42:45 +0300

    Add SPM

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "muparser",
             path: "vendor/muparser",
-            exclude: ["samples"]
+            sources: ["include", "src"]
         )
     ]
 )
